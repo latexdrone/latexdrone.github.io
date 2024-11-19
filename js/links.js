795ac0148bf06bbb063d2138093e48c0bf30ea03
Latexdrone <81490543+Latexdrone@users.noreply.github.com>
2024-11-19 02:45:18 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,58 +1,18 @@
 export const links = [
+        {
+        name: "BlueSky",
+        link: 
+"https://bsky.app/profile/slavedafy.cz",
+        image: 
+"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
+    },
+
     {
         name: "Twitter",
         link: 
-"https://twitter.com/geeksforgeeks",
+"https://twitter.com/slave_Dafy",
         image: 
 "https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
-    {
-        name: "Linkedin",
-        link: 
-"https://www.linkedin.com/company/geeksforgeeks/",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "Instagram",
-        link: 
-"https://www.instagram.com/geeks_for_geeks/?hl=en",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "E-mail",
-        link: 
-"mailto:feedback@geeksforgeeks.org",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "Telegram",
-        link: 
-"https://t.me/geeksforgeeks_official",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "YouTube",
-        link: 
-"https://www.youtube.com/@GeeksforGeeksVideos/featured",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "Discord",
-        link: 
-"https://discord.com/invite/HvJvpjz27z",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
-    {
-        name: "Facebook",
-        link: 
-"https://www.facebook.com/geeksforgeeks.org/",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
-    },
+
 ];
