18d6955511f7c8e4afa91b6b9aef273499a5fa94
Latexdrone <81490543+Latexdrone@users.noreply.github.com>
2024-11-19 02:47:26 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -3,16 +3,12 @@ export const links = [
         name: "BlueSky",
         link: 
 "https://bsky.app/profile/slavedafy.cz",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
 
     {
         name: "Twitter",
         link: 
 "https://twitter.com/slave_Dafy",
-        image: 
-"https://media.geeksforgeeks.org/wp-content/uploads/20230115205457/user1.png",
     },
 
 ];
