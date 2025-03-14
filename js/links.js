a6609c0672fb8587b77ebbddf51d5653a8bf5205
latexdrone <81490543+latexdrone@users.noreply.github.com>
2025-03-14 23:40:02 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,19 +1,19 @@
 export const links = [
-        {
+{
         name: "BlueSky",
-        link: 
-"https://bsky.app/profile/slavedafy.cz",
-    },
-
-    {
+        link: "https://bsky.app/profile/slavedafy.cz",
+},
+{
         name: "Twitter",
-        link: 
-"https://x.com/slave_Dafy",
-    },
+        link: "https://x.com/slave_Dafy",
+},
 {
         name: "Recon",
-        link: 
-"https://www.recon.com/view_profile.aspx?id=4516392",
-    },
+        link: "https://www.recon.com/view_profile.aspx?id=4516392",
+},
+{
+        name: "Instagram",
+        link: "https://www.instagram.com/slavedafy/",
+},
 
 ];
