fc32922de609a87ca8e1ad0c07b3ec3bc1dd8fbd
latexdrone <81490543+latexdrone@users.noreply.github.com>
2025-03-20 08:57:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-import { links } from "./links.js";
+export const links = [
+{
+        name: "BlueSky",
+        link: "https://bsky.app/profile/slavedafy.cz",
+},
+{
+        name: "Twitter",
+        link: "https://x.com/slave_Dafy",
+},
+{
+        name: "Recon",
+        link: "https://www.recon.com/view_profile.aspx?id=4516392",
+},
+{
+        name: "Instagram",
+        link: "https://www.instagram.com/slavedafy/",
+},
+
+];
 
 const linkContainer = document.getElementById("links");
 
